253a5202a914bce0337a1646446f239779b8e86c
tekimmehmet <tekimmehmet@hotmail.com>
2024-05-19 18:22:43 +0300

    Add project files.

---
+++ MT_Superfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MT_Superfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
