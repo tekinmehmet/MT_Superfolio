8f7319ede78d3b858f682fdaabf6116743d8313a
tekimmehmet <tekimmehmet@hotmail.com>
2024-05-21 15:29:01 +0300

    Projelerim KIsmın tamamlandı

--- EntityLayer/Concrete/Portfolio.cs
+++ EntityLayer/Concrete/Portfolio.cs
@@ -13,6 +13,8 @@ namespace EntityLayer.Concrete
         public int PorfolioId { get; set; }
         public string ImageUrl { get; set; }
         public string Name { get; set; }
+        public string ProjectUrl { get; set; }
+        public string ImageUrl2 { get; set; }
 
     }
 }
---
+++ MT_Superfolio/ViewComponents/Portfolio/PorfolioList.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MT_Superfolio.ViewComponents.Portfolio
+{
+    public class PorfolioList:ViewComponent
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = portfolioManager.TGetList();
+            return View(values);
+        }
+    }
+}
